806cd0aceffbe4d43e33f5c4d296fbb9aecf439c
JericHunter <51177847+JericHunter@users.noreply.github.com>
2021-05-13 20:19:55 +0000

    db stuff

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
+  
+require("dotenv").config();
 const { ApolloServer, gql } = require("apollo-server");
+const mongoose = require("mongoose");
 
-//GraphQL Schemas
-const typeDefs = gql`
-  type ToDo {
-    id: ID!
-    title: String!
-    description: String!
-  }
-  input ToDoInput {
-    title: String!
-    description: String!
-  }
-  type Query {
-    getToDo(toDoId: ID!): ToDo!
-    getToDos: [ToDo!]!
-  }
-  type Mutation {
-    createToDo(toDoInput: ToDoInput): ToDo
-    updateToDo(toDoId: ID!, toDoInput: ToDoInput): ToDo
-    deleteToDo(toDoId: ID!): ToDo
-    deleteToDos: [ToDo!]!
-  }
-`;
+//Connect to database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    throw new Error(error);
+  });
+
+//Database Model
+const toDoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
